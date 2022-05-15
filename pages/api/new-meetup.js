f04218327ec11a0ecd3fb7a0180f8a0f56b53cc1
Kush1406 <kushagra1499@gmail.com>
2022-05-15 22:12:52 +0530

    commitMsz

---
+++ pages/api/new-meetup.js
@@ -0,0 +1,28 @@
+import { MongoClient } from "mongodb";
+
+// api/new-meetup
+// POST
+
+async function handler(req, res) {
+  if (req.method === "POST") {
+    const data = req.body;
+
+    const client = await MongoClient.connect(
+      "mongodb+srv://soyuz1499:rJNzDEF3JtNt3JJl@cluster0.eikkv.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
+  }
+}
+
+export default handler;
---
+++ pages/index.js
@@ -0,0 +1,58 @@
+import { MongoClient } from "mongodb";
+import Head from "next/head";
+
+import MeetupList from "../components/meetups/MeetupList";
+
+function HomePage(props) {
+  return (
+    <>
+      <Head>
+        <title>React meetups!</title>
+        <meta
+          name="description"
+          content="Browse a huge list of highly active React meetups!"
+        />
+      </Head>
+      <MeetupList meetups={props.meetups} />
+    </>
+  );
+}
+
+// export async function getServerSideProps(context) {
+//   const req = context.req;
+//   const res = context.res;
+
+//   return {
+//     props: {
+//       meetups: DUMMY_MEETUPS,
+//     },
+//   };
+// }
+
+export async function getStaticProps() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://soyuz1499:rJNzDEF3JtNt3JJl@cluster0.eikkv.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+
+  const db = client.db();
+
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find().toArray();
+
+  client.close();
+
+  return {
+    props: {
+      meetups: meetups.map((meetup) => ({
+        title: meetup.title,
+        address: meetup.address,
+        image: meetup.image,
+        id: meetup._id.toString(),
+      })),
+    },
+    revalidate: 1,
+  };
+}
+
+export default HomePage;
